4ea09bd89f8592afea585cfce92fb8d4b2078bb6
hasankarim18 <hallysust@gmail.com>
2023-05-21 10:08:27 +0600

    vercel problem solved by removing await from clien.connect()

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   // await client.connect();
+    client.connect();
 
      const actionPalsDb = client.db("actionPalsDb");
     const toysCollection = actionPalsDb.collection("toys");
